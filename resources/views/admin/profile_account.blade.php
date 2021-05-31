326aa16d39b296a8f0d7fa10b084f299df05972f
nifz <mochanifz@gmail.com>
2021-05-31 23:49:36 +0700

    updated

--- resources/views/admin/profile_account.blade.php
+++ resources/views/admin/profile_account.blade.php
@@ -147,58 +147,9 @@
             <div class="card">
             <form method="post" class="needs-validation" novalidate="">
                 <div class="card-header">
-                <h4>Sampahku</h4>
+                    <h4>Sampahku</h4>
                 </div>
                 <div class="card-body">
-                    <div class="row">                               
-                    <div class="form-group col-md-6 col-12">
-                        <label>First Name</label>
-                        <input type="text" class="form-control" value="Ujang" required="">
-                        <div class="invalid-feedback">
-                        Please fill in the first name
-                        </div>
-                    </div>
-                    <div class="form-group col-md-6 col-12">
-                        <label>Last Name</label>
-                        <input type="text" class="form-control" value="Maman" required="">
-                        <div class="invalid-feedback">
-                        Please fill in the last name
-                        </div>
-                    </div>
-                    </div>
-                    <div class="row">
-                    <div class="form-group col-md-7 col-12">
-                        <label>Email</label>
-                        <input type="email" class="form-control" value="ujang@maman.com" required="">
-                        <div class="invalid-feedback">
-                        Please fill in the email
-                        </div>
-                    </div>
-                    <div class="form-group col-md-5 col-12">
-                        <label>Phone</label>
-                        <input type="tel" class="form-control" value="">
-                    </div>
-                    </div>
-                    <div class="row">
-                    <div class="form-group col-12">
-                        <label>Bio</label>
-                        <textarea class="form-control summernote-simple">Ujang maman is a superhero name in <b>Indonesia</b>, especially in my family. He is not a fictional character but an original hero in my family, a hero for his children and for his wife. So, I use the name as a user in this template. Not a tribute, I'm just bored with <b>'John Doe'</b>.</textarea>
-                    </div>
-                    </div>
-                    <div class="row">
-                    <div class="form-group mb-0 col-12">
-                        <div class="custom-control custom-checkbox">
-                        <input type="checkbox" name="remember" class="custom-control-input" id="newsletter">
-                        <label class="custom-control-label" for="newsletter">Subscribe to newsletter</label>
-                        <div class="text-muted form-text">
-                            You will get new information about products, offers and promotions
-                        </div>
-                        </div>
-                    </div>
-                    </div>
-                </div>
-                <div class="card-footer text-right">
-                <button class="btn btn-primary">Save Changes</button>
                 </div>
             </form>
             </div>
